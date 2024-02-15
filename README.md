89bb4a16ea9712ae8541a7b7936117a19bd38be4
kasty100 <kaustubhofficials@gmail.com>
2024-02-15 22:11:23 +0530

    First Draft

---
+++ README.md
@@ -0,0 +1,2 @@
+# -Snake-Game-Java_Based-
+This is a good old snake game which is completely java based , Have Fun Keep Coding :D
---
+++ src/GamePanel.java
@@ -0,0 +1,171 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener{
+	
+	 static final int SCREEN_WIDTH = 600;
+	 static final int SCREEN_HEIGHT = 600;
+	 static final int UNIT_SIZE = 15;
+	 static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	 static final int DELAY = 75;
+	 
+	 final int x[]= new int[GAME_UNITS];
+	 final int y[]= new int[GAME_UNITS];
+	 int bodyParts=6;
+	 int miceEaten;
+	 int miceX;
+	 int miceY;
+	 char direction='R';
+	 boolean running=false;
+	 Timer timer;
+	 Random random;
+	 
+	 
+	
+	GamePanel(){
+		random=new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+		
+	}
+	public void startGame() {
+		newMice();
+		running = true;
+		Timer timer=new Timer(DELAY,this);
+		timer.start();
+		 
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
+	public void draw(Graphics g) {
+		
+		
+for(int i =0;i<=SCREEN_HEIGHT/UNIT_SIZE;i++) 
+{
+	g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+	g.drawLine(0,  i*UNIT_SIZE, SCREEN_WIDTH,i*UNIT_SIZE);
+	
+}
+g.setColor(Color.red);
+g.fillOval(miceX, miceY, UNIT_SIZE, UNIT_SIZE);
+for(int i =0;i<bodyParts;i++)
+{
+	if(i==0)
+	{
+		g.setColor(Color.green);
+		g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+	}
+	else 
+	{
+		g.setColor(new Color(45,180,0));
+		g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+	}
+}
+		
+	}
+	public void newMice() {
+		
+		miceX=random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		miceX=random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+		
+		
+		
+		
+	}
+	public void move() {
+		for(int i=bodyParts;i>0;i--)
+		{
+			x[i]=x[i-1];
+			y[i]=y[i-1];
+		}
+		switch(direction)
+		{
+		case 'U':
+			y[0]=y[0]-UNIT_SIZE;
+			break;
+		case 'D':
+			y[0]=y[0]+UNIT_SIZE;
+			break;
+		case 'L':
+			x[0]=x[0]-UNIT_SIZE;
+			break;
+		case 'R':
+			x[0]=x[0]+UNIT_SIZE;
+			break;
+			
+			
+			
+		}
+		
+	}
+	public void checkMice() {
+		
+	}
+	public void checkCollision() {
+		//checks if head collides with body
+				for(int i = bodyParts;i>0;i--) {
+					if((x[0] == x[i])&& (y[0] == y[i])) {
+						running = false;
+					}
+				}
+				//check if head touches left border
+				if(x[0] < 0) {
+					running = false;
+				}
+				//check if head touches right border
+				if(x[0] > SCREEN_WIDTH) {
+					running = false;
+				}
+				//check if head touches top border
+				if(y[0] < 0) {
+					running = false;
+				}
+				//check if head touches bottom border
+				if(y[0] > SCREEN_HEIGHT) {
+					running = false;
+				}
+				
+				if(!running) {
+					timer.stop();
+				}
+	}
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(running) {
+			move();
+			checkMice();
+			checkCollision();
+			
+		}
+		repaint();
+		
+		
+		
+		
+	}
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+		
+	}
+
+}
