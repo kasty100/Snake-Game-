6f55d706dfbe6a19cfed9064473549b9487de263
kasty100 <kaustubhofficials@gmail.com>
2024-02-15 22:32:09 +0530

    Works_on_Key_Adaptors_inputs

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -163,7 +163,29 @@ for(int i =0;i<bodyParts;i++)
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT: 
+				if (direction !='R') {
+					direction='L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT: 
+				if (direction !='L') {
+					direction='R';
+				}
+				break;
+			case KeyEvent.VK_UP: 
+				if (direction !='D') {
+					direction='U';
+				}
+				break;
+			case KeyEvent.VK_DOWN: 
+				if (direction !='U') {
+					direction='D';
+				}
+				break;
+				
+			}
 		}
 		
 	}
